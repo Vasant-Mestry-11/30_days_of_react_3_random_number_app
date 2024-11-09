de5b1bcaf42af1fb711c9efb6428c8b907316ccd
Vasant-Mestry-11 <vasantmestry07@gmail.com>
2024-11-09 12:03:38 +0530

    :zap: :art: :wrench: generate logic changed

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,32 +1,15 @@
 import { useState } from "react";
 import "./Input.css";
 
-// const RESTRICTED_KEY_CODE = [".", "e", "-"];
-
 const Input = () => {
   const [minValue, setMinValue] = useState("");
   const [maxValue, setMaxValue] = useState("");
   const [generatedNumber, setGeneratedNumber] = useState("");
-  // const [isDisable, setDisable] = useState(false);
-
-  // useEffect(() => {
-  //   console.log("called", minValue, maxValue);
-  //   if (minValue <= maxValue || maxValue <= minValue) {
-  //     setDisable(true);
-  //   } else {
-  //     setDisable(false);
-  //   }
-  // }, [minValue, maxValue]);
-
   function handleGenerate() {
-    const minNumber = Math.floor(Math.random() * minValue) + 1;
-    const maxNumber = Math.floor(Math.random() * maxValue) + 1;
-
-    if (maxNumber <= minNumber) {
-      handleGenerate();
-    } else {
-      setGeneratedNumber(maxNumber);
-    }
+    const start = parseInt(minValue);
+    const end = parseInt(maxValue);
+    const randomNumber = Math.floor(Math.random() * (end - start) + start);
+    setGeneratedNumber(randomNumber);
   }
   return (
     <div className="wrapper">
@@ -35,32 +18,17 @@ const Input = () => {
         type="number"
         min="0"
         value={minValue}
-        onChange={(e) => {
-          // if (RESTRICTED_KEY_CODE.includes(e.target.value)) {
-          //   return;
-          // }
-          setMinValue(e.target.value);
-        }}
-        // onKeyDown={(e) => {
-        //   if (RESTRICTED_KEY_CODE.includes(e.key)) {
-        //     return;
-        //   }
-        // }}
+        onChange={(e) => setMinValue(e.target.value)}
         className="wrapper__input"
+        placeholder="Start range"
       />
       <input
         type="number"
         min="0"
         value={maxValue}
-        onChange={(e) => {
-          setMaxValue(e.target.value);
-        }}
-        // onKeyDown={(e) => {
-        //   if (RESTRICTED_KEY_CODE.includes(e.key)) {
-        //     return;
-        //   }
-        // }}
+        onChange={(e) => setMaxValue(e.target.value)}
         className="wrapper__input"
+        placeholder="End range"
       />
       <button
         className="wrapper_button"
